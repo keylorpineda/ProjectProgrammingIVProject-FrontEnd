cde314e8c385366c908a6b0fdab19b646b69cff0
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2026-05-02 00:44:42 -0600

    feat: create interactive parallax login page with framer-motion animations

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -70,7 +70,7 @@ export default function Login() {
       document.removeEventListener("mousemove", handleMouseMove)
       if (rafRef.current !== null) cancelAnimationFrame(rafRef.current)
     }
-  }, [])
+  }, [mouseX, mouseY])
 
 
 
