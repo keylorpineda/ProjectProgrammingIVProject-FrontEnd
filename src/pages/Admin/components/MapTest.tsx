541d87b9a7e0d523f2d088ad146dbd8dee744a1d
Jefferson_Abarca_AI <jabarca840@gmail.com>
2026-05-13 22:10:56 -0600

    feat: implement CampContext and CampsService for managing camp data and state

---
+++ src/pages/Admin/components/MapTest.tsx
@@ -0,0 +1,9 @@
+import MapDashboardWrapper from "@/features/map-test/components/MapDashboardWrapper"
+
+export default function MapTest() {
+  return (
+    <div style={{ width: "100%", height: "100%", minHeight: 0 }}>
+      <MapDashboardWrapper />
+    </div>
+  )
+}
