36903e74a1e4932e32ff9c465490899a34392fe5
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2026-05-30 15:04:32 -0600

    chore: disable subject-case enforcement in commitlint configuration

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         "revert",
       ],
     ],
-    "subject-case": [2, "always", "lower-case"],
+    "subject-case": [0, "always", "lower-case"],
     "subject-max-length": [2, "always", 200],
     "header-max-length": [2, "always", 200],
     "body-max-line-length": [1, "always", 200],
